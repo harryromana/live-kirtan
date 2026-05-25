29ae0b98f199fac6245ccca5655fce0673d72036
harryromana <hinder.romana@gmail.com>
2026-05-25 12:05:18 +0530

    server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
-const request = require('request');
+const https = require('https');
 
 const app = express();
 
 app.get('/kirtan', (req, res) => {
   const url = 'https://live.sgpc.net:8442/';
 
-  request({
-    url: url,
+  https.get(url, {
     headers: {
       'User-Agent': 'Mozilla/5.0',
       'Icy-MetaData': '1'
     }
-  }).pipe(res);
+  }, (stream) => {
+    res.setHeader('Content-Type', 'audio/mpeg');
+    stream.pipe(res);
+  }).on('error', (err) => {
+    res.status(500).send('Stream Error');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
